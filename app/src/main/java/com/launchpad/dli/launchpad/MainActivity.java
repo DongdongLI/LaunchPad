107fc7152adca2c78556645630669d870e1d417d
Dongdong Li (Andy) <lidongdong559@hotmail.com>
2017-03-07 18:15:31 -0500

    Implement the config(shared preference). Allow the app to launch the corresponding apps after receiving guestures.

--- app/src/main/java/com/launchpad/dli/launchpad/MainActivity.java
+++ app/src/main/java/com/launchpad/dli/launchpad/MainActivity.java
@@ -1,15 +1,31 @@
 package com.launchpad.dli.launchpad;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v4.view.MotionEventCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.widget.TextView;
+
+import com.launchpad.dli.launchpad.fragment.ConfigDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     final static String DEBUG_TAG = "mainactivity";
+    public static final String PREFS_NAME = "mypreference";
 
     TextView textView;
 
@@ -24,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
     static int GLOBAL_TOUCH_POSITION_Y = 0;
     static int GLOBAL_TOUCH_CURRENT_POSITION_Y = 0;
 
+    static String twoFingerDragConfig;
+    static String threeFingerDragConfig;
+    static String fourFingerDragConfig;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +55,14 @@ public class MainActivity extends AppCompatActivity {
         originalYTextView = (TextView) findViewById(R.id.originalY);
         currentXTextView = (TextView) findViewById(R.id.currentX);
         currentYTextView = (TextView) findViewById(R.id.currentY);
+
+        refreshConfig();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshConfig();
     }
 
     private boolean handleMultiTouch(MotionEvent event, int fingerCount){
@@ -71,6 +99,22 @@ public class MainActivity extends AppCompatActivity {
                     if(Math.abs(diff)>=500) {
                         actionStr = "it is a " + fingerCount + " finger drag "+calculateDistance();
                         textView.setText(actionStr);
+
+                        switch (fingerCount){
+                            case 2:
+                                String url = "waze://?ll=27.3969313,-82.4425731&navigate=yes";
+                                Intent intent = new Intent( Intent.ACTION_VIEW, Uri.parse( url ) );
+                                startActivity( intent );
+                                break;
+                            case 3:
+                                openApp(this, threeFingerDragConfig);
+                                break;
+                            case 4:
+                                openApp(this, fourFingerDragConfig);
+                                break;
+                            default:
+                                break;
+                        }
                         flag = true;
                     }
                     else{
@@ -90,6 +134,18 @@ public class MainActivity extends AppCompatActivity {
         return flag;
     }
 
+    public static boolean openApp(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        Intent i = manager.getLaunchIntentForPackage(packageName);
+        if (i == null) {
+            Log.d(DEBUG_TAG, "nothing");
+            return false;
+        }
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        context.startActivity(i);
+        return true;
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
@@ -98,6 +154,29 @@ public class MainActivity extends AppCompatActivity {
         handleMultiTouch(event, count);
 
         return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item1:
+                //Toast.makeText(this, "Config", Toast.LENGTH_SHORT).show();
+                showDialog();
+                return true;
+
+            case R.id.menu_item2:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     private void updateStatus(String str) {
@@ -117,4 +196,18 @@ public class MainActivity extends AppCompatActivity {
 
         return Math.sqrt(vertical*vertical + horizontal*horizontal);
     }
+
+    public void showDialog() {
+        DialogFragment newFragment = new ConfigDialog();
+        newFragment.show(getFragmentManager(), "Config");
+    }
+
+    private void refreshConfig() {
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.PREFS_NAME, Context.MODE_PRIVATE);
+
+        twoFingerDragConfig = sharedPreferences.getString("2FingerDrag","empty value");
+        threeFingerDragConfig = sharedPreferences.getString("3FingerDrag","empty value");
+        fourFingerDragConfig = sharedPreferences.getString("4FingerDrag","empty value");
+    }
+
 }
---
+++ app/src/main/java/com/launchpad/dli/launchpad/fragment/ConfigDialog.java
@@ -0,0 +1,77 @@
+package com.launchpad.dli.launchpad.fragment;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.launchpad.dli.launchpad.MainActivity;
+import com.launchpad.dli.launchpad.R;
+
+/**
+ * Created by dli on 3/3/2017.
+ */
+
+public  class ConfigDialog extends DialogFragment {
+
+    EditText twoFingerText;
+    EditText threeFingerText;
+    EditText fourFingerText;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Config");
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view=inflater.inflate(R.layout.config_fragment,null);
+        builder.setView(view);
+
+        twoFingerText = (EditText) view.findViewById(R.id.two_finger_drag_action);
+        threeFingerText = (EditText) view.findViewById(R.id.three_finger_drag_action);
+        fourFingerText = (EditText) view.findViewById(R.id.four_finger_drag_action);
+
+        read();
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                save();
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        return builder.create();
+    }
+
+    private void save() {
+        SharedPreferences.Editor editor = this.getActivity().getSharedPreferences(MainActivity.PREFS_NAME, Context.MODE_PRIVATE).edit();
+        editor.putString("2FingerDrag",twoFingerText.getText().toString());
+        editor.putString("3FingerDrag",threeFingerText.getText().toString());
+        editor.putString("4FingerDrag",fourFingerText.getText().toString());
+        editor.commit();
+    }
+
+    private void read() {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(MainActivity.PREFS_NAME, Context.MODE_PRIVATE);
+
+        twoFingerText.setText(sharedPreferences.getString("2FingerDrag","empty value"));
+        threeFingerText.setText(sharedPreferences.getString("3FingerDrag","empty value"));
+        fourFingerText.setText(sharedPreferences.getString("4FingerDrag","empty value"));
+    }
+}
+
